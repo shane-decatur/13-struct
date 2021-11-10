a6d7694fdec697a1d4018eca0a7cd3c2a909450d
shane-decatur <sdecatur20@stuy.edu>
2021-11-10 00:35:57 -0500

    done

--- main.c
+++ main.c
@@ -13,6 +13,7 @@ struct pop_entry{
 };
 
 int read_csv(){
+  printf("reading nyc_pop.csv");
   int fd = open("nyc_pop.csv",O_RDONLY);
   struct stat info;
   stat("./nyc_pop.csv", &info);
@@ -74,7 +75,7 @@ int read_data(){
     printf("%d: year: %d boro: %s pop: %d\n",count, data[i].year,data[i].boro,data[i].population);
     count++;
   }
-
+  close(fd);
   return 0;
 }
 
@@ -90,11 +91,65 @@ int add_data(){
 
   int fd = open("./nyc_pop.data",O_WRONLY | O_APPEND);
   write(fd,&entry,sizeof(entry));
-  printf("Appended data to file: year: %d\t boro:%s\t pop%d\n",entry.year,entry.boro,entry.population);
+  close(fd);
+  printf("Appended data to file: year: %d\t boro: %s\t pop: %d\n",entry.year,entry.boro,entry.population);
   return 0;
 }
 
-int main(){
-  read_csv();
-  read_data();
+int update_data(){
+  struct stat info;
+  stat("./nyc_pop.data",&info);
+  int fd = open("./nyc_pop.data",O_RDWR);
+  struct pop_entry *data = malloc(info.st_size+sizeof(struct pop_entry));
+  read(fd,data,info.st_size);
+
+  int i;
+  int count = 0;
+  for (i = 0;i*sizeof(struct pop_entry) < info.st_size;i++){
+    printf("%d: year %d boro: %s pop: %d\n",count,data[i].year,data[i].boro,data[i].population);
+    count++;
+  }
+
+  struct pop_entry entry;
+
+  printf("entry to update: ");
+  char input1[100];
+  char input2[100];
+  char boro[15];
+  fgets(input1,100,stdin);
+
+  printf("enter year boro pop: ");
+  fgets(input2,100,stdin);
+
+  sscanf(input2,"%d %s %d\n", &(entry.year),boro,&(entry.population));
+  strcpy(entry.boro,boro);
+  int s = atoi(input1);
+  lseek(fd,s*sizeof(struct pop_entry),SEEK_SET);
+  write(fd,&entry,sizeof(entry));
+  return 0;
+}
+
+int main(int argc, char *argv[]){
+  if (argc != 2){
+    printf("select an option:\n-read_csv\n-read_data\n-add_data\n-update_data\n");
+    return 0;
+  }
+  if (!strcmp(argv[1],"-read_csv")){
+    read_csv();
+  }
+  else if (!strcmp(argv[1],"-read_data")){
+    read_data();
+  }
+  else if (!strcmp(argv[1],"-add_data")){
+    add_data();
+  }
+  else if (!strcmp(argv[1],"-update_data")){
+    update_data();
+  }
+  else{
+    printf("Please enter a valid argument");
+    return 0;
+  }
+  printf("File updated");
+  return 0;
 }
